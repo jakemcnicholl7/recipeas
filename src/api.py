cb714d38d595dc1bbf3856ea9b1289aafdf7a4c8
Jake McNicholl <jakemcnicholl@gmail.com>
2023-10-08 16:54:36 +0100

    Update root API to return random meals

--- src/api.py
+++ src/api.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 
+from src.dependency_config import chef
+
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
-    return jsonify({"Greeting":"Hello World!"})
+def get_random_meals(chef=chef):
+    return chef.make_random_meals()
+
 
 if __name__ == '__main__':
     application.run()
---
+++ src/dependency_config.py
@@ -0,0 +1,8 @@
+from src.chef.chef import Chef
+from src.chef.sous_chef import SpoonacularSousChef
+
+sous_chef = SpoonacularSousChef()
+chef = Chef(sous_chef=sous_chef)
+
+ 
+
