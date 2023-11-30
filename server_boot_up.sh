70c6bd608b12326bf412271a42852c5acf708d54
Jake McNicholl <jakemcnicholl@gmail.com>
2023-11-30 21:01:28 +0000

    Trial appspec.yml deployment hook

--- server_boot_up.sh
+++ server_boot_up.sh
@@ -1,15 +1,26 @@
-PROJECT_TITLE=recipeas
-DEPLOYMENT_DIRECTORY=$pwd
-WORKPLACE=$HOME/workplace
-PROJECT=$WORKPLACE/$PROJECT_TITLE
-WEBSITE="taitneamh.ie"
 
-echo "HERE" >> test_deployment_log.txt
+HOME=/home/ec2-user/
+
+log () {
+	echo $1 >> $HOME/deployment.log
+}
+
+HOME=/home/ec2-user/
+log $HOME 
+DEPLOYMNET_DIRECTORY=/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+log $DEPLOYMENT_DIRECTORY
+WORKPLACE=$HOME/workplace
+log $WORKPLACE
+PROJECT=$WORKPLACE/$APPLICATION_NAME
+log $PROJECT
+WEBSITE="taitneamh.ie"
+log $WEBSITE
+
 
 # Load bashrc
 source $HOME/.bashrc
 
 # Workplace Setup
 rm -r $WORKPLACE
-mkdir -p $WORKPLACE
-ln -sf $DEPLOYMENT_DIRECTORY $WORKPLACE 
+mkdir -p $PROJECT
+ln -sf $DEPLOYMENT_DIRECTORY $PROJECT
