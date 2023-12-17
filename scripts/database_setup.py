11084324e7059200a955a565556cc4ab76ae1619
root <root@ip-172-31-17-133.eu-west-1.compute.internal>
2023-12-17 16:39:49 +0000

    Add database setup file

---
+++ scripts/database_setup.py
@@ -0,0 +1,68 @@
+import pymongo
+import pandas as pd
+import sys
+import os
+
+MONGO_ENDPOINT = "mongodb://localhost:27017/"
+RECIPEAS_DATABASE = "recipeas"
+RECIPE_COLLECTION = "recipes"
+RECIPE_DB_SOURCE_FILE = "data/recipes.csv"
+
+RESET = False
+
+
+def main():
+    setup_recipe_db()
+
+
+def setup_recipe_db():
+    if not os.path.exists(RECIPE_DB_SOURCE_FILE):
+        print("[ERROR] No database source file exists")
+        sys.exit(1)
+
+    client = pymongo.MongoClient(MONGO_ENDPOINT) 
+    
+    db = client[RECIPEAS_DATABASE]
+
+    if RESET:
+        reset_db(db)
+
+    if is_db_already_setup(db):
+        print("[INFO] Database is already set up")
+        sys.exit(0)
+
+    insert_data(db)
+
+
+def reset_db(db):
+    collection = db[RECIPE_COLLECTION]
+    collection.drop()
+        
+        
+def insert_data(db):
+    collection = db[RECIPE_COLLECTION]
+
+    source_db = pd.read_csv(RECIPE_DB_SOURCE_FILE)
+    source_data = source_db.to_dict(orient="records")
+    total_data = len(source_data)
+
+    index = 0
+    insert_size = 5
+
+    while index < total_data:
+        insert_start = index
+        insert_end = min(index + insert_size, total_data)
+        
+        data_insert = source_data[insert_start: insert_end]
+        collection.insert_many(data_insert)
+
+        index = insert_end
+
+
+def is_db_already_setup(db):
+    return RECIPE_COLLECTION in db.list_collection_names()
+
+
+if __name__ == "__main__":
+    main()
+
