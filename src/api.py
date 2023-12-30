597d052ebb9f4f8c8444890f72b858f818eb6886
Jake McNicholl <jakemcnicholl@gmail.com>
2023-12-30 15:07:47 +0000

    Add CORS logic enabling the local browser on port 3000 to make requests to the backend endpoint

--- src/api.py
+++ src/api.py
@@ -1,8 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from src.dependency_config import chef
 
 application = Flask(__name__)
+
+# CORS
+# Allows the browser running on localhost:3000 to make requests to this backend endpoint 
+
+CORS(application, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @application.route('/')
 def get_random_meals(chef=chef):
